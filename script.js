da4c63f1fe0d0a8270ecd1b9c7534ed7ffde4980
Tahlia <tbolden13@gmail.com>
2022-09-17 19:46:14 -0700

    working on index html file time slots code

---
+++ script.js
@@ -0,0 +1,15 @@
+var timeDisplayEl = $('#time-display');
+
+
+
+
+
+
+
+
+
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+  
