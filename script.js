a8e43b21f4d90ae8a912d145c8f5c063f61a2b7e
Tahlia <tbolden13@gmail.com>
2022-09-18 02:34:09 -0700

    trying to fix the current time and time color block code errors

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+/* this time display is an id so not sure why the # is not working on the code. look into*/
 var timeDisplayEl = $('#time-display');
-var saveBtn = $(".saveBtn");
+var saveBtn = $('.saveBtn');
 
 
 
@@ -10,7 +12,7 @@ var saveBtn = $(".saveBtn");
 
 
     var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-   $("#currentDay").html(currentTime);
+   $('#currentDay').html(currentTime); 
   
   //this function is for the past/present/future time blocks in planner
 function colorCode() {
@@ -19,12 +21,34 @@ function colorCode() {
     var timeNow = parseInt($(this).attr("id").split("hour")[1]);
 
     if (hour< timeNow) {
-      $(this).addClass("future").removeClass("past present");
+      $(this).addClass(".past").removeClass("past present");
     }
     else if (hour > timeNow) {
-      $(this).addClass("present").removeClass("past future")
+      $(this).addClass(".future").removeClass("past future")
     }
-  })
+    else if (hour === timeNow) {
+    $(this).addClass(".present").removeClss("past future")
+  }
+})
 };
 colorCode();
 
+
+saveBtn.on("click", function() {
+ var timePlan = $(this).siblings(".hour").text();
+  var planText = $(this).siblings(".description").val();
+  localStorage.setItem(timePlan, planText);
+});
+
+function getPlan() {
+$(".hour").each(function() {
+  var timeNow =$(this).text();
+  var planInfo = localStorage.getItem(timeNow);
+
+  if(planInfo !==null) {
+    $(this).siblings(".description").val(planInfo);
+  }
+});
+}
+
+getPlan();
