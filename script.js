9d3f3083c595ef21750bc63dc29172247bdc4401
Tahlia <tbolden13@gmail.com>
2022-09-17 21:45:23 -0700

    working on js for the planner (timeblock colors) time still not current

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var timeDisplayEl = $('#time-display');
+var saveBtn = $(".saveBtn");
 
 
 
@@ -8,8 +9,22 @@ var timeDisplayEl = $('#time-display');
 
 
 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+   $("#currentDay").html(currentTime);
   
+  //this function is for the past/present/future time blocks in planner
+function colorCode() {
+  var hour = moment().hour();
+  $(".time-block").each(function() {
+    var timeNow = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (hour< timeNow) {
+      $(this).addClass("future").removeClass("past present");
+    }
+    else if (hour > timeNow) {
+      $(this).addClass("present").removeClass("past future")
+    }
+  })
+};
+colorCode();
+
